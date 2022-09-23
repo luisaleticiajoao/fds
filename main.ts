5430c1c4c09010411275b019e203b744dcd05f61
luisaleticiajoao <113694900+luisaleticiajoao@users.noreply.github.com>
2022-09-23 09:17:16 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+input.onPinPressed(TouchPin.P2, function () {
+    p2 = 1
+})
+input.onPinPressed(TouchPin.P1, function () {
+    p1 = 1
+})
+let p1 = 0
+let p2 = 0
+basic.showNumber(123)
+basic.showString("start!")
+basic.forever(function () {
+	
+})
